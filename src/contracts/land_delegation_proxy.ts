08c9af21fa3514d2780a439b35529206cb19fa90
alexx855 <alexx855@users.noreply.github.com>
2024-03-18 00:39:57 +0000

    Weekly update

--- src/contracts/land_delegation_proxy.ts
+++ src/contracts/land_delegation_proxy.ts
@@ -71,85 +71,15 @@ const abi = [
 		type: 'fallback',
 	},
 	{
-		inputs: [],
-		name: 'admin',
-		outputs: [
-			{
-				internalType: 'address',
-				name: 'admin_',
-				type: 'address',
-			},
-		],
-		stateMutability: 'nonpayable',
-		type: 'function',
-	},
-	{
-		inputs: [
-			{
-				internalType: 'address',
-				name: 'newAdmin',
-				type: 'address',
-			},
-		],
-		name: 'changeAdmin',
-		outputs: [],
-		stateMutability: 'nonpayable',
-		type: 'function',
-	},
-	{
-		inputs: [],
-		name: 'implementation',
-		outputs: [
-			{
-				internalType: 'address',
-				name: 'implementation_',
-				type: 'address',
-			},
-		],
-		stateMutability: 'nonpayable',
-		type: 'function',
-	},
-	{
-		inputs: [
-			{
-				internalType: 'address',
-				name: 'newImplementation',
-				type: 'address',
-			},
-		],
-		name: 'upgradeTo',
-		outputs: [],
-		stateMutability: 'nonpayable',
-		type: 'function',
-	},
-	{
-		inputs: [
-			{
-				internalType: 'address',
-				name: 'newImplementation',
-				type: 'address',
-			},
-			{
-				internalType: 'bytes',
-				name: 'data',
-				type: 'bytes',
-			},
-		],
-		name: 'upgradeToAndCall',
-		outputs: [],
-		stateMutability: 'payable',
-		type: 'function',
-	},
-	{
 		stateMutability: 'payable',
 		type: 'receive',
 	},
 ] as const
 const LAND_DELEGATION_PROXY: Contract<typeof abi> = {
 	name: 'Land Delegation Proxy',
-	address: '0x4ec37b7f8fce7cd2a96839b3c97f3d034ffb52c2',
-	is_deprecated: true,
-	updated_at: 1710117544,
+	address: '0x6e1c680981a101bf6dc794e1d3ba7fa496eea12e',
+	is_deprecated: false,
+	updated_at: 1710722264,
 	abi: abi,
 }
 export default LAND_DELEGATION_PROXY
