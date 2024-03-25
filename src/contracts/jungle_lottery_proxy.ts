d7bbe1a28e2bc52c9f783b3898855629c8eb24f3
alexx855 <alexx855@users.noreply.github.com>
2024-03-25 00:40:10 +0000

    Weekly update

---
+++ src/contracts/jungle_lottery_proxy.ts
@@ -0,0 +1,87 @@
+import { Contract } from '@/contract'
+const abi = [
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_logic',
+				type: 'address',
+			},
+			{
+				internalType: 'address',
+				name: '_admin',
+				type: 'address',
+			},
+			{
+				internalType: 'bytes',
+				name: '_data',
+				type: 'bytes',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'constructor',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: false,
+				internalType: 'address',
+				name: 'previousAdmin',
+				type: 'address',
+			},
+			{
+				indexed: false,
+				internalType: 'address',
+				name: 'newAdmin',
+				type: 'address',
+			},
+		],
+		name: 'AdminChanged',
+		type: 'event',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'beacon',
+				type: 'address',
+			},
+		],
+		name: 'BeaconUpgraded',
+		type: 'event',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'implementation',
+				type: 'address',
+			},
+		],
+		name: 'Upgraded',
+		type: 'event',
+	},
+	{
+		inputs: [],
+		stateMutability: 'payable',
+		type: 'fallback',
+	},
+	{
+		inputs: [],
+		stateMutability: 'payable',
+		type: 'receive',
+	},
+] as const
+const JUNGLE_LOTTERY_PROXY: Contract<typeof abi> = {
+	name: 'Jungle Lottery Proxy',
+	address: '0x083a79beeafc368a8c97df63726ed54cdb26556a',
+	is_deprecated: false,
+	updated_at: 1711327157,
+	abi: abi,
+}
+export default JUNGLE_LOTTERY_PROXY
---
+++ src/contracts/kongz_extra_data_proxy.ts
@@ -0,0 +1,87 @@
+import { Contract } from '@/contract'
+const abi = [
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_logic',
+				type: 'address',
+			},
+			{
+				internalType: 'address',
+				name: '_admin',
+				type: 'address',
+			},
+			{
+				internalType: 'bytes',
+				name: '_data',
+				type: 'bytes',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'constructor',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: false,
+				internalType: 'address',
+				name: 'previousAdmin',
+				type: 'address',
+			},
+			{
+				indexed: false,
+				internalType: 'address',
+				name: 'newAdmin',
+				type: 'address',
+			},
+		],
+		name: 'AdminChanged',
+		type: 'event',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'beacon',
+				type: 'address',
+			},
+		],
+		name: 'BeaconUpgraded',
+		type: 'event',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'implementation',
+				type: 'address',
+			},
+		],
+		name: 'Upgraded',
+		type: 'event',
+	},
+	{
+		inputs: [],
+		stateMutability: 'payable',
+		type: 'fallback',
+	},
+	{
+		inputs: [],
+		stateMutability: 'payable',
+		type: 'receive',
+	},
+] as const
+const KONGZ_EXTRA_DATA_PROXY: Contract<typeof abi> = {
+	name: 'Kongz Extra Data Proxy',
+	address: '0x57cd000b215b0a93f01e6ac47dd992a8361d9ad9',
+	is_deprecated: false,
+	updated_at: 1711327157,
+	abi: abi,
+}
+export default KONGZ_EXTRA_DATA_PROXY
