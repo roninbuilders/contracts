f0ad83d57abfcebf8ebc7b239364be807b3ad3e1
alexx855 <alexx855@users.noreply.github.com>
2024-04-15 03:07:20 +0000

    Weekly update

---
+++ src/contracts/guardian_pending_array.ts
@@ -0,0 +1,444 @@
+import { Contract } from '@/contract'
+const abi = [
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_lockable',
+				type: 'address',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'constructor',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'guardian',
+				type: 'address',
+			},
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'user',
+				type: 'address',
+			},
+		],
+		name: 'GuardianRenounce',
+		type: 'event',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'guardian',
+				type: 'address',
+			},
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'user',
+				type: 'address',
+			},
+		],
+		name: 'GuardianSet',
+		type: 'event',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'pendingGuardian',
+				type: 'address',
+			},
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'user',
+				type: 'address',
+			},
+		],
+		name: 'PendingGuardianSet',
+		type: 'event',
+	},
+	{
+		inputs: [],
+		name: 'LOCKABLE',
+		outputs: [
+			{
+				internalType: 'contract ILockERC721',
+				name: '',
+				type: 'address',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_protege',
+				type: 'address',
+			},
+		],
+		name: 'acceptGuardianship',
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_user',
+				type: 'address',
+			},
+		],
+		name: 'getLockedAssetsOfUsers',
+		outputs: [
+			{
+				internalType: 'uint256[]',
+				name: 'lockedAssets',
+				type: 'uint256[]',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_user',
+				type: 'address',
+			},
+			{
+				internalType: 'uint256',
+				name: '_startIndex',
+				type: 'uint256',
+			},
+			{
+				internalType: 'uint256',
+				name: '_maxLen',
+				type: 'uint256',
+			},
+		],
+		name: 'getLockedAssetsOfUsers',
+		outputs: [
+			{
+				internalType: 'uint256[]',
+				name: 'lockedAssets',
+				type: 'uint256[]',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_guardian',
+				type: 'address',
+			},
+		],
+		name: 'getPendingProteges',
+		outputs: [
+			{
+				internalType: 'address[]',
+				name: '_proteges',
+				type: 'address[]',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_guardian',
+				type: 'address',
+			},
+		],
+		name: 'getProtegesFromGuardian',
+		outputs: [
+			{
+				internalType: 'address[]',
+				name: 'proteges',
+				type: 'address[]',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+		],
+		name: 'guardianToUserIndex',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		name: 'guardianToUsers',
+		outputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+		],
+		name: 'guardianUserCount',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+		],
+		name: 'guardians',
+		outputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'uint256[]',
+				name: '_tokenIds',
+				type: 'uint256[]',
+			},
+		],
+		name: 'lockMany',
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		name: 'pendingGuardianArray',
+		outputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+		],
+		name: 'pendingGuardianUserCount',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+		],
+		name: 'pendingGuardians',
+		outputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_guardian',
+				type: 'address',
+			},
+		],
+		name: 'proposeGuardian',
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_protege',
+				type: 'address',
+			},
+		],
+		name: 'renounce',
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_protege',
+				type: 'address',
+			},
+			{
+				internalType: 'uint256[]',
+				name: '_tokenIds',
+				type: 'uint256[]',
+			},
+			{
+				internalType: 'address',
+				name: '_recipient',
+				type: 'address',
+			},
+		],
+		name: 'renounceAndTransfer',
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'uint256[]',
+				name: '_tokenIds',
+				type: 'uint256[]',
+			},
+		],
+		name: 'unlockMany',
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'uint256[]',
+				name: '_tokenIds',
+				type: 'uint256[]',
+			},
+			{
+				internalType: 'address',
+				name: '_recipient',
+				type: 'address',
+			},
+		],
+		name: 'unlockManyAndTransfer',
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+		],
+		name: 'userData',
+		outputs: [
+			{
+				internalType: 'address',
+				name: 'guardian',
+				type: 'address',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+] as const
+const GUARDIAN_PENDING_ARRAY: Contract<typeof abi> = {
+	name: 'Guardian Pending Array',
+	address: '0x64f58013256075ae41ac7c0a8936b7e758892a47',
+	is_deprecated: false,
+	updated_at: 1713150327,
+	abi: abi,
+}
+export default GUARDIAN_PENDING_ARRAY
