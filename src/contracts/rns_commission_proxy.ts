3fdcc1cc73f0248e8a789408be0cf0ad11819876
alexx855 <alexx855@users.noreply.github.com>
2024-07-15 00:47:08 +0000

    Weekly update

---
+++ src/contracts/rns_commission_proxy.ts
@@ -0,0 +1,85 @@
+import { Contract } from '@/contract'
+const abi = [
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'logic',
+				type: 'address',
+			},
+			{
+				internalType: 'address',
+				name: 'admin',
+				type: 'address',
+			},
+			{
+				internalType: 'bytes',
+				name: 'data',
+				type: 'bytes',
+			},
+		],
+		stateMutability: 'payable',
+		type: 'constructor',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: false,
+				internalType: 'address',
+				name: 'previousAdmin',
+				type: 'address',
+			},
+			{
+				indexed: false,
+				internalType: 'address',
+				name: 'newAdmin',
+				type: 'address',
+			},
+		],
+		name: 'AdminChanged',
+		type: 'event',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'beacon',
+				type: 'address',
+			},
+		],
+		name: 'BeaconUpgraded',
+		type: 'event',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'implementation',
+				type: 'address',
+			},
+		],
+		name: 'Upgraded',
+		type: 'event',
+	},
+	{
+		stateMutability: 'payable',
+		type: 'fallback',
+	},
+	{
+		stateMutability: 'payable',
+		type: 'receive',
+	},
+] as const
+const RNS_COMMISSION_PROXY: Contract<typeof abi> = {
+	name: 'RNS Commission Proxy',
+	address: '0x1bece3a948c14eefbaace67fe6f51cd21b79aa21',
+	is_deprecated: false,
+	created_at: 1720085010,
+	abi: abi,
+}
+export default RNS_COMMISSION_PROXY
---
+++ src/contracts/ronin_config_proxy.ts
@@ -0,0 +1,80 @@
+import { Contract } from '@/contract'
+const abi = [
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_logic',
+				type: 'address',
+			},
+			{
+				internalType: 'bytes',
+				name: '_data',
+				type: 'bytes',
+			},
+		],
+		stateMutability: 'payable',
+		type: 'constructor',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: false,
+				internalType: 'address',
+				name: 'previousAdmin',
+				type: 'address',
+			},
+			{
+				indexed: false,
+				internalType: 'address',
+				name: 'newAdmin',
+				type: 'address',
+			},
+		],
+		name: 'AdminChanged',
+		type: 'event',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'beacon',
+				type: 'address',
+			},
+		],
+		name: 'BeaconUpgraded',
+		type: 'event',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'implementation',
+				type: 'address',
+			},
+		],
+		name: 'Upgraded',
+		type: 'event',
+	},
+	{
+		stateMutability: 'payable',
+		type: 'fallback',
+	},
+	{
+		stateMutability: 'payable',
+		type: 'receive',
+	},
+] as const
+const RONIN_CONFIG_PROXY: Contract<typeof abi> = {
+	name: 'Ronin Config Proxy',
+	address: '0xd5298601cbe23304942cc78f2c148ee0416f22b4',
+	is_deprecated: false,
+	created_at: 1720869838,
+	abi: abi,
+}
+export default RONIN_CONFIG_PROXY
