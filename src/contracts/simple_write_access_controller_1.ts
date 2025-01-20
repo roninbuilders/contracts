bffce20ebdbc3d5936ec0626c14e16892ec70ddc
alexx855 <alexx855@users.noreply.github.com>
2025-01-20 01:11:17 +0000

    Weekly update

---
+++ src/contracts/simple_write_access_controller_1.ts
@@ -0,0 +1,202 @@
+import { Contract } from '@/contract'
+const abi = [
+	{
+		inputs: [],
+		stateMutability: 'nonpayable',
+		type: 'constructor',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: false,
+				internalType: 'address',
+				name: 'user',
+				type: 'address',
+			},
+		],
+		name: 'AddedAccess',
+		type: 'event',
+	},
+	{
+		anonymous: false,
+		inputs: [],
+		name: 'CheckAccessDisabled',
+		type: 'event',
+	},
+	{
+		anonymous: false,
+		inputs: [],
+		name: 'CheckAccessEnabled',
+		type: 'event',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'from',
+				type: 'address',
+			},
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'to',
+				type: 'address',
+			},
+		],
+		name: 'OwnershipTransferRequested',
+		type: 'event',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'from',
+				type: 'address',
+			},
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'to',
+				type: 'address',
+			},
+		],
+		name: 'OwnershipTransferred',
+		type: 'event',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: false,
+				internalType: 'address',
+				name: 'user',
+				type: 'address',
+			},
+		],
+		name: 'RemovedAccess',
+		type: 'event',
+	},
+	{
+		inputs: [],
+		name: 'acceptOwnership',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_user',
+				type: 'address',
+			},
+		],
+		name: 'addAccess',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'checkEnabled',
+		outputs: [
+			{
+				internalType: 'bool',
+				name: '',
+				type: 'bool',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'disableAccessCheck',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'enableAccessCheck',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_user',
+				type: 'address',
+			},
+			{
+				internalType: 'bytes',
+				name: '',
+				type: 'bytes',
+			},
+		],
+		name: 'hasAccess',
+		outputs: [
+			{
+				internalType: 'bool',
+				name: '',
+				type: 'bool',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'owner',
+		outputs: [
+			{
+				internalType: 'address payable',
+				name: '',
+				type: 'address',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_user',
+				type: 'address',
+			},
+		],
+		name: 'removeAccess',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_to',
+				type: 'address',
+			},
+		],
+		name: 'transferOwnership',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+] as const
+const SIMPLE_WRITE_ACCESS_CONTROLLER_1: Contract<typeof abi> = {
+	name: 'Simple Write Access Controller',
+	address: '0x5ccd985e2e2ab9ab717acf6fc53307e6339f3ad3',
+	is_deprecated: false,
+	created_at: 1736846870,
+	abi: abi,
+}
+export default SIMPLE_WRITE_ACCESS_CONTROLLER_1
